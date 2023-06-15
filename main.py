9362ffed70a82dc5af982ab73dfc0b69840155e7
AndraDC <116729974+AndraDC@users.noreply.github.com>
2023-06-16 06:27:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,10 @@ dataTerpilih = data_covid[(data_covid['Date'] >= pd.to_datetime(tanggalTerpilih[
 jenis_kasus = st.selectbox('Pilih Jenis Kasus', ['New Cases', 'New Deaths', 'New Recovered','New Active Cases', 'Total Cases',
                                                  'Total Deaths', 'Total Recovered', 'Total Active Cases' ])
 
-p = figure(title=f'Jumlah {jenis_kasus} per Tanggal {tanggalTerpilih[0]} hingga {tanggalTerpilih[1]}', x_axis_type='datetime', plot_width=800, plot_height=400)
+p = figure(title=f'Jumlah {jenis_kasus} per Tanggal {tanggalTerpilih[0]} hingga {tanggalTerpilih[1]}', x_axis_type='datetime', 
+           x_axis_label='Tanggal', x_axis_label='Jumlah Kasus' plot_width=800, plot_height=400)
 source = ColumnDataSource(data=dataTerpilih)
+#p.line('Date', jenis_kasus, source=source, line_width=2)
 p.line('Date', jenis_kasus, source=source, line_width=2)
 
 selected_indices = []
