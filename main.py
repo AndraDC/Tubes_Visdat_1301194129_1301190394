65a6f07c0874c73618894015716f479f6e702424
AndraDC <116729974+AndraDC@users.noreply.github.com>
2023-06-16 14:40:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,13 @@ p = figure(title=f'Jumlah {jenis_kasus} per Tanggal {tanggalTerpilih[0]} hingga 
 source = ColumnDataSource(data=dataTerpilih)
 p.line('Date', jenis_kasus, source=source, line_width=2)
 
-
+data_grouped = dataTerpilih.groupby('Province')[jenis_kasus].sum().reset_index()
+prov = data_grouped['Province']
+b = figure(title='', x_range=prov, plot_width=800, plot_height=500)
+sc = ColumnDataSource(data=data_grouped)
+b.vbar(x='Province', top=jenis_kasus, source=sc, width=0.9)
+b.xaxis.major_label_orientation = "vertical"
+b.yaxis.formatter = NumeralTickFormatter(format='0,0')
 
 selected_indices = []
 
@@ -51,11 +57,11 @@ p.on_event(Tap, plot_tap_event)
 hover1 = HoverTool(tooltips=[('Tanggal', '@Date{%F}'), (jenis_kasus, '@{jenis_kasus}')], formatters={'@Date': 'datetime'})
 p.add_tools(hover1)
 
-#hover2 = HoverTool(tooltips=[(jenis_kasus, '@{jenis_kasus}')])
-#b.add_tools(hover2)
+hover2 = HoverTool(tooltips=[(jenis_kasus, '@{jenis_kasus}')])
+b.add_tools(hover2)
 
 st.bokeh_chart(p, use_container_width=True)
-#st.bokeh_chart(b, use_container_width=True)
+st.bokeh_chart(b, use_container_width=True)
 
 if selected_indices:
     data_terpilih = dataTerpilih.iloc[selected_indices]
