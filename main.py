5f2176033a233232fed99b2ff585e1af152c9bb1
AndraDC <116729974+AndraDC@users.noreply.github.com>
2023-06-16 09:37:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,23 +21,18 @@ tanggalAkhir = data_covid['Date'].max().date()
 
 
 
-col1, col2 = st.columns(2)
-with col1:
-  tanggalTerpilih = st.slider('Pilih Tanggal', tanggalMulai, tanggalAkhir, (tanggalMulai, tanggalAkhir))
-  data_terfilter = data_covid[(data_covid['Date']).dt.date == tanggalTerpilih]
-  tanggalTerpilih = (tanggalTerpilih[0]), (tanggalTerpilih[1])
-  dataTerpilih = data_covid[(data_covid['Date'] >= pd.to_datetime(tanggalTerpilih[0])) & (data_covid['Date'] <= pd.to_datetime(tanggalTerpilih[1]))]
-  
-  jenis_kasus = st.selectbox('Pilih Jenis Kasus', ['New Cases', 'New Deaths', 'New Recovered','New Active Cases', 'Total Cases',
+tanggalTerpilih = st.slider('Pilih Tanggal', tanggalMulai, tanggalAkhir, (tanggalMulai, tanggalAkhir))
+data_terfilter = data_covid[(data_covid['Date']).dt.date == tanggalTerpilih]
+tanggalTerpilih = (tanggalTerpilih[0]), (tanggalTerpilih[1])
+dataTerpilih = data_covid[(data_covid['Date'] >= pd.to_datetime(tanggalTerpilih[0])) & (data_covid['Date'] <= pd.to_datetime(tanggalTerpilih[1]))]
+jenis_kasus = st.selectbox('Pilih Jenis Kasus', ['New Cases', 'New Deaths', 'New Recovered','New Active Cases', 'Total Cases',
                                                  'Total Deaths', 'Total Recovered', 'Total Active Cases' ])
 
-with col2:
-  with st.container():
-    p = figure(title=f'Jumlah {jenis_kasus} per Tanggal {tanggalTerpilih[0]} hingga {tanggalTerpilih[1]}', x_axis_type='datetime', 
+p = figure(title=f'Jumlah {jenis_kasus} per Tanggal {tanggalTerpilih[0]} hingga {tanggalTerpilih[1]}', x_axis_type='datetime', 
            x_axis_label='Tanggal', y_axis_label='Jumlah Kasus', plot_width=800, plot_height=400)
-    source = ColumnDataSource(data=dataTerpilih)
-    p.line('Date', jenis_kasus, source=source, line_width=2)
-    st.bokeh_chart(p, use_container_width=True)
+source = ColumnDataSource(data=dataTerpilih)
+p.line('Date', jenis_kasus, source=source, line_width=2)
+st.bokeh_chart(p, use_container_width=True)
   
 
 
