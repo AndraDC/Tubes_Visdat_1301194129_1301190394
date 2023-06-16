a5f93e5094b7816fded5ffece0d10ca249665bd6
AndraDC <116729974+AndraDC@users.noreply.github.com>
2023-06-16 15:44:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ p.on_event(Tap, plot_tap_event)
 b.on_event(Tap, plot_tap_event)
 
 hover1 = HoverTool(tooltips=[('Tanggal', '@Date{%F}'), (jenis_kasus, '@{jenis_kasus}')],
-                   formatters={'@Date': 'datetime', '@{jenis_kasus}': 'printf'})
+                   formatters={'@Date': 'datetime', '@{jenis_kasus}': 'numeral'})
 p.add_tools(hover1)
 
 hover2 = HoverTool(tooltips=[(jenis_kasus, '@{jenis_kasus}')])
